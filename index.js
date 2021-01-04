52f970bc12332c4f7847cf1a5dcf260d45a7687c
elskater98 <45320338+elskater98@users.noreply.github.com>
2021-01-04 17:16:52 +0100

    Add MongoDB connection

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/* Mongodb Connection*/
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/database',{useNewUrlParser: true,useFindAndModify:false,useCreateIndex:true,useUnifiedTopology:true})
+
+// Check connection
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'Failed connection.'));
+db.once('open',function () {
+    console.log("Successfully connection.")
+});
+
+/*Server*/
 app.listen(port , () => {
   console.log("Hello World!")
 })
